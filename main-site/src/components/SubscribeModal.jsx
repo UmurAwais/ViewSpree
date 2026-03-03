8ea8c31d64834a05de4b70a7d0aac3760394957c
Umar Awais <umarawais.co@gmail.com>
2026-03-03 14:37:40 +0500

    set the z-index for the subscription modal

--- main-site/src/components/SubscribeModal.jsx
+++ main-site/src/components/SubscribeModal.jsx
@@ -13,7 +13,7 @@ const SubscribeModal = ({ isOpen, onClose }) => {
   }, []);
 
   return (
-    <div className="fixed inset-0 z-100 flex items-center justify-center p-4">
+    <div className="fixed inset-0 z-1000 flex items-center justify-center p-4">
       {/* Backdrop */}
       <motion.div 
         initial={{ opacity: 0 }}
